2d38b951e0b145310418639a4060b6e5953ad62a
Erfan Gholizade <erfantkerfan@yahoo.com>
2025-12-23 17:07:34 +0330

    implement graceful server shutdown

--- cmd/serve.go
+++ cmd/serve.go
@@ -5,7 +5,11 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
 	"strings"
+	"syscall"
+	"time"
 
 	"github.com/docker/docker/api/types/swarm"
 	"github.com/docker/docker/client"
@@ -63,7 +67,31 @@ var serveCmd = &cobra.Command{
 
 		fmt.Println("version: " + version)
 		fmt.Println("trying to bind to " + ServerHost + ":" + ServerPort)
-		return router.Run(ServerHost + ":" + ServerPort)
+
+		srv := &http.Server{
+			Addr:    ServerHost + ":" + ServerPort,
+			Handler: router,
+		}
+
+		go func() {
+			if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+				fmt.Fprintf(os.Stderr, "listen error: %s\n", err)
+			}
+		}()
+
+		quit := make(chan os.Signal, 1)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		<-quit
+		fmt.Println("\nshutting down server...")
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := srv.Shutdown(ctx); err != nil {
+			return fmt.Errorf("server forced to shutdown: %w", err)
+		}
+
+		fmt.Println("server exited gracefully")
+		return nil
 	},
 }
 
